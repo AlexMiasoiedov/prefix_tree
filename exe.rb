327f60a8d0b429fb2b3dfa61447b0ad2a0bb43b9
AlexMiasoiedov <alexerna8@gmail.com>
2015-05-01 19:04:25 +0300

    list_method_v1

--- exe.rb
+++ exe.rb
@@ -1,10 +1,13 @@
-load 'lib/tree/node.rb'
-load 'lib/tree.rb'
-
+require_relative 'lib/tree'
+p Tree::Node
 test=Tree.new
-puts 'Write the word: '
-str=gets.chomp
-test.add(str)
-puts 'Search the word: '
-str=gets.chomp
-test.include?(str)
+puts 'Add word "test"'
+puts 'Add word "tesla"'
+puts 'Add word "temp"'
+test.add('test')
+test.add('tesla')
+test.add('temp')
+puts "\nIs the 'test' include?"
+p test.include?('test')
+p 'Return all with prefix "te"'
+test.list('te')
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,35 +1,57 @@
+require_relative 'tree/node'
 class Tree
+  WORD_MIN_LENGTH = 5
+
   def initialize
-    @root = Node.new#('')
+    @root = Node.new
   end
 
   def add (str)
-    @current = @root
-    @arr = str.split('')
-    @arr.each do |ch|
-      @next = @current.children[ch]
-      if @next.nil?
-        @next = Node.new
-        @current.children[ch] = @next
+    current = @root
+    str.split('').each do |ch|
+      next_node = current.children[ch]
+      if next_node.nil?
+        next_node = Node.new
+        current.children[ch] = next_node
       end
-      @current = @next
+      current = next_node
     end
-    @current.is_word = true
+    current.is_word = true
   end
 
   def include? (str)
-    @current = @root
-    @arr = str.split('')
-    @arr.each do |ch|
-      @child = @current.children[ch]
-      if @child.nil?
-        puts false
-        break
-      end
-      @current = @current.children[ch]
+    current = @root
+    str.split('').each do |ch|
+      child = current.children[ch]
+      return false if child.nil?
+      current = current.children[ch]
     end
-    if @current.is_word
-      puts true
+    if current.is_word
+      return true
+    else
+      return false
     end
+  end
+
+  def get_prefix (str)
+    current = @root
+    str.split('').each do |ch|
+      current = current.children[ch]
+    end
+    return current
+  end
+
+  def some_method (object_hash)
+    p "object hash #{object_hash}"
+    object_hash.each do |key, value|
+      current = object_hash[key]
+      p key
+      some_method (current.children)
+    end
+  end
+
+  def list (str='')
+    current = get_prefix (str)
+    some_method (current.children)
   end
 end
