953718c173ead043acfb5b1b650a88b896592f91
AlexMiasoiedov <alexerna8@gmail.com>
2015-06-22 11:25:39 +0300

    for approving

--- spec/features/user_capybara_spec.rb
+++ spec/features/user_capybara_spec.rb
@@ -14,7 +14,15 @@ RSpec.describe '#tree front-end', :type => :feature do
     end
     it 'should NOT have unexicting content' do
       expect(page).to have_no_content 'fake content'
-      #page.save_screenshot('homepage.png')
+    end
+    it "should be clear in the inpust filds" do
+      within("form#list-form") do
+        
+        expect("input#text-area").to have_content nil
+      end
+      within("form#list-form") do
+        expect("input#text-ar").to have_content nil
+      end
     end
   end
 
@@ -23,25 +31,31 @@ RSpec.describe '#tree front-end', :type => :feature do
       within("form#add-form") do
         fill_in 'text-area', :with => 'new_word'
       end
-      #page.save_screenshot('fill new_word.png')
       click_button 'add'
-      #page.save_screenshot('add new_word.png')
       within("div#list-output") do
         expect(page).to have_content 'new_word'
       end
-      #page.save_screenshot('add new_word.png')
     end
     it 'nothing happens when used click on add button with out filling added word' do
       click_button 'add'
       expect(page).to have_content nil
-      #page.save_screenshot('add multiple click.png')
     end
     it 'nothing happens when used click multiple time on add button with out filling added word' do
+      within("form#list-form") do
+        expect("input#text-area").to have_content nil
+      end
       click_button 'add'
       click_button 'add'
       click_button 'add'
       expect(page).to have_content nil
-      #page.save_screenshot('add multiple click.png')
+    end
+    it "should be clear in the inpust filds" do
+      within("form#list-form") do
+        expect("input#text-area").to have_content nil
+      end
+      within("form#list-form") do
+        expect("input#text-ar").to have_content nil
+      end
     end
   end
 
@@ -52,15 +66,13 @@ RSpec.describe '#tree front-end', :type => :feature do
       expect(page).to have_content 'testtabdh'
       expect(page).to have_content 'someone'
       expect(page).to have_no_content 'fake content'
-      #page.save_screenshot('list click with out prefix.png')
     end
     it 'user fill specific prefix and click on list button' do
       within("form#list-form") do
-        fill_in 'text-ar', :with => 'w'
+        fill_in 'text-ar', :with => 'testt'
       end
       click_button 'list'
       expect(page).to have_content 'testtree'
-     #page.save_screenshot('list click with existing prefix.png')
     end
     it 'user fill specific unexisting prefix and click on list button' do
       within("form#list-form") do
@@ -68,7 +80,15 @@ RSpec.describe '#tree front-end', :type => :feature do
       end
       click_button 'list'
       expect(page).to have_content nil
-      #page.save_screenshot('list click with unexisting prefix.png')
+    end
+    it "should be clear in the inpust filds" do
+      within("form#list-form") do
+
+        expect("input#text-area").to have_content nil
+      end
+      within("form#list-form") do
+        expect("input#text-ar").to have_content nil
+      end
     end
   end
 
@@ -76,7 +96,15 @@ RSpec.describe '#tree front-end', :type => :feature do
     it 'page should have content from read file' do
       click_button 'read'
       expect(page).to have_content 'spec_read_file'
-      #page.save_screenshot('read click.png')
+    end
+    it "should be clear in the inpust filds" do
+      within("form#list-form") do
+        
+        expect("input#text-area").to have_content nil
+      end
+      within("form#list-form") do
+        expect("input#text-ar").to have_content nil
+      end
     end
   end
 
@@ -84,14 +112,21 @@ RSpec.describe '#tree front-end', :type => :feature do
     it 'page shoul have content from read_zip file' do
       click_button 'read-zip'
       expect(page).to have_content 'spec_read_zip'
-      #page.save_screenshot('read zip click.png')
+    end
+    it "should be clear in the inpust filds" do
+      within("form#list-form") do
+        
+        expect("input#text-area").to have_content nil
+      end
+      within("form#list-form") do
+        expect("input#text-ar").to have_content nil
+      end
     end
   end
 
   describe '#user click on "write" button', :js => true do
     it 'write file should contain content from tree' do
       click_button 'write'
-      #page.save_screenshot('write click.png')
       File.open('spec/files/write', 'r') { |file| file.each_line { |word| expect(page).to have_content word.chomp.gsub(/[\\\"]/,"") } }
     end
     it 'should nothing happens with page content when user click on "write" button' do
@@ -103,7 +138,6 @@ RSpec.describe '#tree front-end', :type => :feature do
       expect(page).to have_content 'spec_read_file'
       expect(page).to have_content 'spec_read_zip'
       expect(page).to have_no_content 'fake content'
-      #page.save_screenshot('write click.png')
     end
     it 'should nothing happens with page content when user click on "write" button multiple times' do
       click_button 'write'
@@ -116,14 +150,21 @@ RSpec.describe '#tree front-end', :type => :feature do
       expect(page).to have_content 'spec_read_file'
       expect(page).to have_content 'spec_read_zip'
       expect(page).to have_no_content 'fake content'
-      #page.save_screenshot('write multiple click.png')
+    end
+    it "should be clear in the inpust filds" do
+      within("form#list-form") do
+        
+        expect("input#text-area").to have_content nil
+      end
+      within("form#list-form") do
+        expect("input#text-ar").to have_content nil
+      end
     end
   end
 
   describe '#user click on "write zip" button', :js => true do
     it 'write file should contain content from tree' do
       click_button 'write-zip'
-      #page.save_screenshot('write zip click.png')
       Zip::File.open('spec/files/write.zip') { |zf| zf.read('write').each_line { |word| expect(page).to have_content word.chomp.gsub(/[\\\"]/,"") }}
     end
     it 'should nothing happens with page content when user click on "write zip" button' do
@@ -135,7 +176,6 @@ RSpec.describe '#tree front-end', :type => :feature do
       expect(page).to have_content 'spec_read_file'
       expect(page).to have_content 'spec_read_zip'
       expect(page).to have_no_content 'fake content'
-      #page.save_screenshot('write zip click.png')
     end
     it 'should nothing happens with page content when user click on "write zip" button multiple times' do
       click_button 'write-zip'
@@ -148,7 +188,15 @@ RSpec.describe '#tree front-end', :type => :feature do
       expect(page).to have_content 'spec_read_file'
       expect(page).to have_content 'spec_read_zip'
       expect(page).to have_no_content 'fake content'
-      #page.save_screenshot('write zip multiple click.png')
+    end
+    it "should be clear in the inpust filds" do
+      within("form#list-form") do
+        
+        expect("input#text-area").to have_content nil
+      end
+      within("form#list-form") do
+        expect("input#text-ar").to have_content nil
+      end
     end
   end
 end
