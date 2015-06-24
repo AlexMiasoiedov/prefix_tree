ec2835736f95cd0c17ac74874d74c32da3beda08
AlexMiasoiedov <alexerna8@gmail.com>
2015-06-24 12:47:07 +0300

    add behaviour that you can NOT add existing word

--- app/public/controller.js
+++ app/public/controller.js
@@ -1,23 +1,24 @@
       $(document).ready(function (){
 
         list_request();
-        $("#add-form").on('submit', function(event) {
+        $("form#add-form").on('submit', function(event) {
           event.preventDefault();
           var added_word = document.getElementById("add-text-field").value;
           var pref = $("b#pref").text();
-          if (added_word != '') {
+          //word_exist(added_word)
+          if (added_word != '' && word_exist(added_word)) {
             add_request();
             if(pref != '') {
-              if (added_word.slice(0, pref.length) == pref) $('#list-output').append("<li>" + added_word + "</li>");
+              if (added_word.slice(0, pref.length) == pref) $('div#list-output').append("<li>" + added_word + "</li>");
             } else {
-              $('#list-output').append("<li>" + added_word + "</li>");
+              $('div#list-output').append("<li>" + added_word + "</li>");
             };
           message = "<b>" + added_word + "</b> was added!";
           notifications(message);
           };
         });
 
-        $("#list-form").on('submit', function(event) {
+        $("form#list-form").on('submit', function(event) {
           event.preventDefault();
           var prefix = document.getElementById("list-text-field").value;
           if(prefix != ''){
@@ -33,28 +34,28 @@
           list_request();
         });
 
-        $("#write").on('click', function(event) {
+        $("input#write").on('click', function(event) {
           event.preventDefault();
           write_write_zip_request("/write_file");
           message = "words was recorded to file";
           notifications(message);
         });
 
-        $("#write-zip").on('click', function(event) {
+        $("input#write-zip").on('click', function(event) {
           event.preventDefault();
           write_write_zip_request("/write_zip")
           message = "words was recorded to zip file";
           notifications(message);
         });
 
-        $("#read").on('click', function(event) {
+        $("input#read").on('click', function(event) {
           event.preventDefault();
           read_read_zip_word("/read_file")
           message = "Words from read file was added into tree";
           notifications(message);
         });
 
-        $("#read-zip").on('click', function(event) {
+        $("input#read-zip").on('click', function(event) {
           event.preventDefault();
           read_read_zip_word("/read_zip")
           message = "Words from read zip file was added into tree";
@@ -69,8 +70,8 @@
             async: false
           });
           list_request();
-          $("#add-form").find('input:text').val('');
-          $("#list-form").find('input:text').val('');
+          $("form#add-form").find('input:text').val('');
+          $("form#list-form").find('input:text').val('');
         });
       });
 
@@ -78,9 +79,9 @@
         $.ajax({
           url: "/add",
           type: "GET",
-          data: $("#add-form").serialize()
+          data: $("form#add-form").serialize()
         });
-        $("#add-form").find('input:text').val('');
+        $("form#add-form").find('input:text').val('');
       };
 
       function list_request() {
@@ -117,9 +118,9 @@
       function parse_json_resp(json) {
         console.log(json);
             var arr_from_json = JSON.parse(json);
-            $('#list-output').empty();
+            $('div#list-output').empty();
             $.each(arr_from_json, function(index, value) {
-              $('#list-output').append("<li>" + value + "</li>");
+              $('div#list-output').append("<li>" + value + "</li>");
             });
       };
 
@@ -127,4 +128,12 @@
         $("div#notific").html(content);
         $("div#notific").slideDown();
         $("div#notific").fadeOut(1700);
+      };
+
+      function word_exist(added_word) {
+        var div_child = div_child = $("div#list-output").children();
+        for (i = 0; i < div_child.length; i++){
+          if (added_word == $(div_child[i]).text()) return false;
+        };
+        return true;
       };
